1235a5b4cfc434f77fb94705d102ec2c5873e008
sharankarthikyan <43072292+sharankarthikyan@users.noreply.github.com>
2021-05-31 09:37:47 +0530

    Final deployement

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends Component {
     console.log(filteredMonsters);
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={(e) => this.setState({ searchField: e.target.value })}
